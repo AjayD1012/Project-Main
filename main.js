43e6c0426dab296a6c51dd8bb9fc6854bd3958c8
AjayD1012 <ajaydadke10@gmail.com>
2024-03-22 12:01:49 +0530

    basic commit

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import './style.css'
 
+import products from "./api/products.json";
+import { showProductContainer } from './homeProductCards';
+
+// Defining a function named as "showProductContainer" that takes an array of products as input.
+
+showProductContainer(products);
